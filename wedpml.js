7cbebf8f85cdfc470a70533ad142b1ddbe1c2aa4
Creator8bit <syoriousewind@gmail.com>
2026-06-07 10:12:25 +0800

    Update wedpml.js

--- wedpml.js
+++ wedpml.js
@@ -406,3 +406,24 @@ function foundCamp(id) {
   }
 }
 
+let current = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach(s => s.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+function nextSlide() {
+  if (current < slides.length - 1) {
+    current++;
+    showSlide(current);
+  }
+}
+
+function prevSlide() {
+  if (current > 0) {
+    current--;
+    showSlide(current);
+  }
+}
